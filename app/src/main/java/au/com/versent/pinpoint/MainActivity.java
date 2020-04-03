234f384b794af335ef0feb5905ce8490881a070c
Laura Brandon <laurakbrandon@gmail.com>
2020-04-03 15:43:27 +1100

    chore(android): upgrade to use androidx

--- app/src/main/java/au/com/versent/pinpoint/MainActivity.java
+++ app/src/main/java/au/com/versent/pinpoint/MainActivity.java
@@ -1,13 +1,15 @@
 package au.com.versent.pinpoint;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
